3b7975072fdeaed18f2b8cab998f29bce8fcbd3d
MargauxDelporte <margauxdelporte@gmail.com>
2025-04-09 16:31:45 -0400

    Update

---
+++ VanillaScenarioLMTriangle.R
@@ -0,0 +1,150 @@
+### High dimension linear model
+rm(list = ls())
+
+mywd='C:/Users/mde4023/OneDrive - Weill Cornell Medicine/0 Projects/FDR_Datasplitting'
+setwd(mywd)
+source('HelperFunctions.R')
+source('TriangleLinRegTrainMS.R')
+source(paste0(mywd,'/Functions Dai/knockoff.R'))
+source(paste0(mywd,'/Functions Dai/analysis.R'))
+source(paste0(mywd,'/Functions Dai/MBHq.R'))
+source(paste0(mywd,'/Functions Dai/DS.R'))
+source(paste0(mywd,'/Functions Dai/fdp_power.R'))
+
+#devtools::install_github("Jeremy690/DSfdr/DSfdr",force = TRUE)
+
+library(MASS)
+library(glmnet)
+library(knockoff)
+library(mvtnorm)
+library(hdi)
+library(dplyr)
+#library(DSfdr)
+library(ggplot2)
+# library(igraph)
+library(RColorBrewer)
+library(openxlsx)
+library(ggpubr)
+library(viridis)
+
+### algorithmic settings
+num_split <- 10
+n <-1500
+p <- 250
+p0 <- 25
+q <- 0.1
+#set.seed(124)(123) i=5
+set.seed(456)
+signal_index <- sample(c(1:p), size = p0, replace = F)
+
+
+Compare_SignalStrenght=function(i,s){
+  set.seed(s)
+  ResultsDataFrame=data.frame()
+  delta <- i
+  X <- mvrnorm(n, mu = rep(0, p), Sigma = diag(p))
+  
+  ### randomly generate the true beta i=4
+  beta_star <- rep(0, p)
+  beta_star[signal_index] <- rnorm(p0, mean = 0, sd = delta*sqrt(log(p)/n))
+  
+  ### generate y
+  y <- X%*%beta_star + rnorm(n, mean = 0, sd = 1)
+  
+  ###my own methods:
+  g=ApplyTriangleLinRegTrain(X=as.data.frame(X), y, q=0.1,num_split=10, signal_index=signal_index, amountTrain=0.333, myseed = 1)
+  
+  ResultsDataFrame=c('LinReg DS',i, as.numeric(g$DS_fdp),as.numeric(g$DS_power))
+  ResultsDataFrame=rbind(ResultsDataFrame,c('LinReg MS',i, as.numeric(g$MDS_fdp),as.numeric(g$MDS_power)))
+  
+  ### Competition
+  DS_result <- DS(X,y, num_split=10, q=0.1)
+  ResultsDataFrame=rbind(ResultsDataFrame,c('DataSplitting',i,DS_result$DS_fdp,DS_result$DS_power))
+  ResultsDataFrame=rbind(ResultsDataFrame,c('MultipleDataSplitting',i,DS_result$MDS_fdp,DS_result$MDS_power))
+  
+  knockoff_result <- knockoff(X, y, q=0.1)
+  ResultsDataFrame=rbind(ResultsDataFrame,c('Knockoff',i,knockoff_result$fdp,knockoff_result$power))
+  
+  BH_result <- MBHq(X, y, q=0.1, num_split)
+  ResultsDataFrame=rbind(ResultsDataFrame,c('BH',i,BH_result$fdp,BH_result$power))
+  
+  ### save data
+  return(ResultsDataFrame)}
+
+Results=data.frame()
+for(s in 1:25){
+  for(i in seq(from=5,to=13,by=1)){
+    Results=rbind(Results,Compare_SignalStrenght(i,s))
+    print(Results)
+  }
+}
+names(Results)
+
+names(Results)=c('Method','SignalStrength', 'FDR','Power')
+
+write.xlsx(Results,file='VanillaResults.xlsx')
+#Results=read.xlsx('Vanilla.xlsx')
+
+####Visualise the results
+# Create a color palette based on the number of unique methods
+
+#setwd('C:/Users/mde4023/Documents/GitHub/Mirror_DS_MSE/VanillaScenarioSignalStrength')
+#write.xlsx(x=as.data.frame(Results),file='Results_signal_NoCorr.xlsx')
+
+colors <- c("#FF0000", "#00FF00", "#0000FF", "#000000", "#FF00FF", )
+sort(unique(Results2$Method))
+colors <- c("#000000","#FF00FF","#009900", "#99ccff", "#0000FF", "#FF0000")
+Results2=Results
+Results2$FDR=round(as.numeric(Results2$FDR),3)
+Results2$Power=round(as.numeric(Results2$Power),2)
+#Results2=subset(Results2,Method %in% c('XGBoost PC','DataSplitting','MultipleDataSplitting','Knockoff','BH'))
+resultsagg <- Results2 %>%
+  group_by(Method, SignalStrength) %>%
+  summarize(
+    Avg_FDR = mean(FDR),
+    Avg_Power = mean(Power)
+  )
+View(resultsagg)
+resultsagg$Signal_noisy <- as.numeric(resultsagg$SignalStrength) + runif(nrow(resultsagg), -0.2, 0.2)
+
+PowerPlot <- ggplot(resultsagg, aes(x = Signal_noisy, y = as.numeric(Avg_Power), color = Method)) +
+  geom_point(size = 3) +
+  geom_line()+
+  labs(x = "Signal", y = "Power") +
+  scale_x_continuous(breaks = seq(from = 5, to = 13, by = 1)) +
+  geom_hline(yintercept = 0.8) +
+  scale_color_manual(values = colors)
+FDRPlot=ggplot(resultsagg, aes(x = Signal_noisy, y = as.numeric(Avg_FDR ), color = Method)) +
+  geom_point(size = 3) +
+  geom_line()+
+  labs(x = "Signal", y = "FDP")+
+  scale_x_continuous(breaks=seq(from=5,to=13,by=1))+
+  geom_hline(yintercept=0.1)+
+  scale_color_manual(values = colors)
+PlotPermute=ggarrange(
+  PowerPlot, FDRPlot,
+  common.legend = TRUE, legend = "right"
+)
+PlotPermute
+
+
+PowerPlot <- ggplot(Results2[1:66,], aes(x = Signal_noisy, y = as.numeric(Power), color = Method)) +
+  geom_point(size = 3) +
+  geom_line()+
+  labs(x = "Signal", y = "Power") +
+  scale_x_continuous(breaks = seq(from = 3, to = 13, by = 1)) +
+  geom_hline(yintercept = 0.8) +
+  scale_color_manual(values = colors)
+FDRPlot=ggplot(Results2[1:66,], aes(x = Signal_noisy, y = as.numeric(FDR ), color = Method)) +
+  geom_point(size = 3) +
+  geom_line()+
+  labs(x = "Signal", y = "FDP")+
+  scale_x_continuous(breaks=seq(from=3,to=13,by=1))+
+  geom_hline(yintercept=0.1)+
+  scale_color_manual(values = colors)
+PlotPermute=ggarrange(
+  PowerPlot, FDRPlot,
+  common.legend = TRUE, legend = "right"
+)
+PlotPermute
+
