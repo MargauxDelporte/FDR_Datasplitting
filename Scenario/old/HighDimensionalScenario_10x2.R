47472de399f44f3fbb64d2bfeff555a3d7bb530a
MargauxDelporte <margauxdelporte@gmail.com>
2025-05-08 12:06:09 -0400

    update

--- Scenario/old/HighDimensionalScenario_10x2.R
+++ Scenario/old/HighDimensionalScenario_10x2.R
@@ -1,9 +1,8 @@
 ### High dimension linear model
 rm(list = ls())
-getwd()
+
 #mywd='C:/Users/mde4023/OneDrive - Weill Cornell Medicine/0 Projects/FDR_Datasplitting'
 mywd='C:/Users/mde4023/Downloads/FDR_Datasplitting'
-#mywd='~/FDR_Datasplitting'
 
 setwd(mywd)
 
@@ -18,13 +17,23 @@ source(paste0(mywd,'/Functions Dai/DS.R'))
 source(paste0(mywd,'/Functions Dai/fdp_power.R'))
 
 #devtools::install_github("Jeremy690/DSfdr/DSfdr",force = TRUE)
+library(xgboost)
+library(gbm)
+library(ranger)
+library(MASS)
+library(neuralnet)
+
 library(glmnet)
 library(knockoff)
 library(mvtnorm)
 library(hdi)
-library(MASS)
+
 ### algorithmic settings
-num_split <- 10
+<<<<<<< HEAD:Scenario/old/HighDimensionalScenario_10x.R
+num_split <- 1
+=======
+num_split <- 2
+>>>>>>> 9b2172635ea7582b8a4fa7ad29cdd1b75d6a6fe7:Scenario/HighDimensionalScenario_10x.R
 n <-1500
 p <- 2000
 p0 <- 25
@@ -47,7 +56,7 @@ Compare_SignalStrength <- function(i, s) {
   # run your custom methods
   g1 <- ApplyTriangleLassoHD(X = X, y = y, q = q, num_split = num_split,
                              signal_index = signal_index, myseed = 1)     
-  
+
   # FDR methods
   DS_result      <- DS(          X = X, y = y, q = q, num_split = num_split)
   knockoff_result<- knockoff(    X = X, y = y, q = q)
@@ -65,23 +74,31 @@ Compare_SignalStrength <- function(i, s) {
   # bind all rows
   ResultsDataFrame <- rbind(
     ResultsDataFrame,
-    data.frame(seed=s, Method = "Boost DS",                Delta = i, FDP = g1$DS_fdp,    Power = g1$DS_power),
-    data.frame(seed=s, Method = "Boost MS",                Delta = i, FDP = g1$MDS_fdp,   Power = g1$MDS_power),
-    data.frame(seed=s, Method = "DataSplitting",           Delta = i, FDP = DS_result$DS_fdp,  Power = DS_result$DS_power),
-    data.frame(seed=s, Method = "MultipleDataSplitting",   Delta = i, FDP = DS_result$MDS_fdp, Power = DS_result$MDS_power),
-    data.frame(seed=s, Method = "Knockoff",                Delta = i, FDP = knockoff_result$fdp, Power = knockoff_result$power),
-    data.frame(seed=s, Method = "Benjamini–Hochberg (BH)", Delta = i, FDP = BH_result$fdp,     Power = BH_result$power)
+    data.frame(Method = "Boost DS",                Delta = i, FDP = g1$DS_fdp,    Power = g1$DS_power),
+    data.frame(Method = "Boost MS",                Delta = i, FDP = g1$MDS_fdp,   Power = g1$MDS_power),
+    data.frame(Method = "DataSplitting",           Delta = i, FDP = DS_result$DS_fdp,  Power = DS_result$DS_power),
+    data.frame(Method = "MultipleDataSplitting",   Delta = i, FDP = DS_result$MDS_fdp, Power = DS_result$MDS_power),
+    data.frame(Method = "Knockoff",                Delta = i, FDP = knockoff_result$fdp, Power = knockoff_result$power),
+    data.frame(Method = "Benjamini–Hochberg (BH)", Delta = i, FDP = BH_result$fdp,     Power = BH_result$power)
   )
-  print(ResultsDataFrame)
+  
   return(ResultsDataFrame)
 }
+
+##check how long one iteration takes 
+system.time({
+  Compare_SignalStrength(7,7)
+  # Replace this block with the code you want to time
+  Sys.sleep(1)  # This just waits for 1 second
+})
+
 
 
 library(parallel)
 
 # Source helper and method files
 
-source(file.path(mywd, 'Functions', 'TriangleLassoHD.R'))
+source(file.path(mywd, 'Functions', 'TriangleBoosterTrainMS.R'))
 source(file.path(mywd, 'Functions', 'HelperFunctions.R'))
 
 # Dai’s routines
@@ -92,36 +109,35 @@ source(file.path(mywd, 'Functions Dai', 'DS.R'))
 source(file.path(mywd, 'Functions Dai', 'fdp_power.R'))
 
 # Load required packages
-pkgs <- c('MASS','glmnet','knockoff','mvtnorm','hdi',
+pkgs <- c('xgboost','gbm','ranger','MASS','glmnet','knockoff','mvtnorm','hdi',
           'foreach','doParallel')
 lapply(pkgs, library, character.only = TRUE)
 
 # === PARAMETER GRID ===
 param_grid <- expand.grid(
-  s = 7:25,
+  s = 1:50,
   i = seq(from = 7, to = 13, by = 1)
 )
-param_grid
 
 # === SET UP PARALLEL BACKEND ===
-cl <- makeCluster(5)#detectCores() - 4)
+cl <- makeCluster(detectCores() - 4)
 # export working dir so workers can source
 clusterExport(cl, 'mywd')
 # have each worker source & load libraries
 clusterEvalQ(cl, {
   setwd(mywd)
-  source(file.path(mywd, 'Functions', 'TriangleLassoHD.R'))
+  source(file.path(mywd, 'Functions', 'TriangleBoosterTrainMS.R'))
   source(file.path(mywd, 'Functions', 'HelperFunctions.R'))
   source(file.path(mywd, 'Functions Dai', 'knockoff.R'))
   source(file.path(mywd, 'Functions Dai', 'analysis.R'))
   source(file.path(mywd, 'Functions Dai', 'MBHq.R'))
   source(file.path(mywd, 'Functions Dai', 'DS.R'))
   source(file.path(mywd, 'Functions Dai', 'fdp_power.R'))
-  lapply(c('MASS','glmnet','knockoff','mvtnorm','hdi','foreach','doParallel'),
+  lapply(c('xgboost','gbm','ranger','MASS','glmnet','knockoff','mvtnorm','hdi'),
          library, character.only = TRUE)
 })
 registerDoParallel(cl)
-
+5991.06
 # === RUN IN PARALLEL AND WRITE OUT ===
 results_list <- foreach(
   k = seq_len(nrow(param_grid)),
@@ -131,48 +147,44 @@ results_list <- foreach(
   s_val <- param_grid$s[k]
   i_val <- param_grid$i[k]
   
-  chunk <- Compare_SignalStrength(i = i_val, s = s_val)
   # compute chunk of results
+  chunk <- Compare_SignalStrength(i = i_val, s = s_val)
+  
+  # write out this chunk immediately
   fname <- sprintf("Results_s%02d_i%02d.csv", s_val, i_val)
   write.csv(chunk, file = fname, row.names = FALSE)
+  
+  # return for final binding
+  chunk
 }
-
 
 # === CLEANUP AND FINAL SAVE ===
 stopCluster(cl)
 warnings()
 # combine all and save full dataset
 Results <- results_list
+write.csv(Results, file = "HighDimensionalScenario_10x_seed1_50.csv", row.names = FALSE)
 
 
-param_grid <- expand.grid(
-  s = 1:50,
-  i = seq(from = 7, to = 13, by = 1)
-)
-k=1
-results=c()
-for(k in 1:nrow(param_grid)){
-  # compute chunk of results
-  s_val <- param_grid$s[k]
-  i_val <- param_grid$i[k]
-  fname <- sprintf("C:/Users/mde4023/Downloads/FDR_Datasplitting/Results HD/Results_s%02d_i%02d.csv", s_val, i_val)
-  rf=read.csv(file = fname)
-  results=rbind(results,rf)
-}
-library(openxlsx)
-#write.xlsx(results,'C:/Users/mde4023/Downloads/FDR_Datasplitting/Results/results_HD.xlsx',rowNames=F)
 
 ##########visualise the results###########
 library(ggplot2)
 library(dplyr)
 library(ggpubr)
 library(readr)
+mywd <- 'C:/Users/mde4023/OneDrive - Weill Cornell Medicine/0 Projects/FDR_Datasplitting/Results'
+mywd <- paste0(mywd,'/Results')
 
-Results=results
+N1 <- read_csv(paste0(mywd,'/NonlinearScenario_10x_seed26_50.csv'))
+names(N1)=c('Method','SignalStrength','FDP','Power')
+N2 <- read_csv(paste0(mywd,'/NonlinearScenario_10x.csv'))
+names(N2)=c('Method','SignalStrength','FDP','Power')
+
+Results=rbind(N1,N2)
 #Results=read.xlsx('VanillaResults.xlsx')
 colors <- c("#000000","#FF00FF","#009900", "#99ccff", "#0000FF", "#FF0000")
 Results2=Results
-names(Results2)=c('seed','Method','SignalStrength','FDP','Power')
+names(Results2)=c('Method','SignalStrength','FDP','Power')
 Results2$FDP=round(as.numeric(Results2$FDP),3)
 Results2$Power=round(as.numeric(Results2$Power),2)
 resultsagg <- Results2 %>%
@@ -213,3 +225,6 @@ PlotPermute=ggarrange(
   common.legend = TRUE, legend = "right"
 )
 PlotPermute
+
+
+
