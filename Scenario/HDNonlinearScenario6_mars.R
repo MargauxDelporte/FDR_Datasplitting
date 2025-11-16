59e6a53a60b3bcadca140d606cf6039135438977
MargauxDelporte <margauxdelporte@gmail.com>
2025-11-16 17:29:46 -0500

    gg

--- Scenario/HDNonlinearScenario6_mars.R
+++ Scenario/HDNonlinearScenario6_mars.R
@@ -59,7 +59,7 @@ Compare_SignalStrength <- function(i, s,other=T) {
   X2 <- matrix(rnorm(n2*p, mean=-1), n2, p)
   X  <- rbind(X1, X2)
   beta_star <- numeric(p)
-  beta_star[signal_index] <- rnorm(p0, 0, delta*sqrt(log(p)/n))*1000
+  beta_star[signal_index] <- rnorm(p0, 0, delta*sqrt(log(p)/n))*10
   y <- (X^2 %*% beta_star+ rnorm(n))
   
   # run your custom methods
@@ -95,7 +95,7 @@ Compare_SignalStrength <- function(i, s,other=T) {
   }
   return(ResultsDataFrame)
 }
-Compare_SignalStrength(10,10,F)
+Compare_SignalStrength(7,7,F)
 Compare_SignalStrength(15,15,F)
 # build grid
 param_grid <- expand.grid(
@@ -104,7 +104,7 @@ param_grid <- expand.grid(
 )
 
 # make sure output dir exists
-out_dir <- file.path(mywd, "Temp")
+out_dir <- file.path(mywd, "Temp2")
 dir.create(out_dir, recursive = TRUE, showWarnings = FALSE)
 
 # iterate over ROWS, not columns
